4b7f5290c16c2844e7d39f33a7f7c2feff252b37
sonuparjapat <sahil992001parjapat@gmail.com>
2023-04-29 11:27:43 +0530

    changes

--- Controls/Controls.js
+++ Controls/Controls.js
@@ -8,9 +8,9 @@ const userRouter=express.Router()
 userRouter.get("/getall",auth,async(req,res)=>{
     try{
         const data=await userModel.find()
-res.status(200).json({"data":data})
+res.status(200).json({"data":data,"status":"success"})
     }catch(err){
-        res.status(400).send({"err":err.message})
+        res.status(400).send({"err":err.message,"status":"error"})
     }
 })
 
@@ -20,7 +20,7 @@ userRouter.post("/register",async(req,res)=>{
     try{
         const data= await userModel.findOne({email})
         if(data){
-            res.status(400).json({msg:"Already Registered"})
+            res.status(400).json({msg:"Already Registered","status":"error"})
         }
         else{
 
@@ -28,11 +28,11 @@ userRouter.post("/register",async(req,res)=>{
         bcrypt.hash(password, 5, async(err, hash) =>{
             const newuser=new userModel({name,email,password:hash})
             await newuser.save()
-            res.status(200).json({msg:"registerd successfully"})
+            res.status(200).json({msg:"registerd successfully","status":"success"})
         });
         }
     }catch(err){
-        res.status(400).json({msg:err.message})
+        res.status(400).json({msg:err.message,"status":"error"})
     }
 })
 userRouter.post("/login",async(req,res)=>{
@@ -44,21 +44,21 @@ try{
         bcrypt.compare(password, user.password, async(err, result)=> {
             if(result){
                 var token = jwt.sign({ authorId:user._id }, 'masai',{ expiresIn: 60 * 5 });
-                res.status(200).send({"msg":"login successfully","token":token})
+                res.status(200).send({"msg":"login successfully","token":token,"status":"success"})
             }
             else{
-                res.status(400).json({msg:"wrong password"})
+                res.status(400).json({msg:"wrong password","status":"error"})
             }
            
         })
       
        
     }else{
-        res.status(400).json({"msg":"wrong credentials"})
+        res.status(400).json({"msg":"wrong credentials","status":"error"})
     }
     
 }catch(err){
-    res.status(400).json({"msg":err.message})
+    res.status(400).json({"msg":err.message,"status":"error"})
 }
 })
 
